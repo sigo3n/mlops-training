efb682a3b1ae7fcb689095876af15230aa021edb
Hendrie Gunawan <sigo3n@gmail.com>
2026-08-12 12:44:07 +0700

    add tracking uri agar langsung ke port 5000

--- eksperimen1.py
+++ eksperimen1.py
@@ -9,6 +9,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     df.drop("target", axis=1), df["target"], test_size=0.2, random_state=42
 )
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("training-pagi")
 with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42).fit(X_train, y_train)
